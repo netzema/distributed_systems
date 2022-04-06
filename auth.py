8a2e2f237ca5547450584b913e8d9456a5bff988
Daniel Netzl <netzl.daniel@gmail.com>
2022-04-06 16:12:42 +0200

    access validation

--- auth.py
+++ auth.py
@@ -10,10 +10,16 @@ users = {"admin": {"password":"admin", "role":"admin"}}
 def create_token():
     return '%030x' % random.randrange(16**30)
 
+def validate_access(name):
+    if users[name]["token"]:
+        return users[name]["role"]
 
 class User(Resource):
     def get(self, name):
-        return {name: users[name]}
+        if name in users:
+            return {name: users[name]}
+        else:
+            return {'success': False, 'error_msg': f'User {name} not found.'}
 
     def put(self, name):
         user_information = {}
@@ -23,30 +29,36 @@ class User(Resource):
         return {name: users[name]}
 
     def post(self, name):
-        user_information = {}
-        user_information["password"] = request.form["password"]
-        user_information["role"] = request.form["role"]
-        users[name] = user_information
-        return {name: users[name]}
+        # only for admins
+        if validate_access(name) == 'admin':
+            user_information = {}
+            username = request.form["username"]
+            user_information["password"] = request.form["password"]
+            user_information["role"] = request.form["role"]
+            users[username] = user_information
+            return {username: users[username]}
+        else:
+            return {'success': False, 'error_msg': 'No authentification for that action.'}
 
     def delete(self, name):
-        if name in users:
-            del users[name]
-            return {"success": True}
-        else:
-            return {"success": False, "error_msg": "User not found"}
+        if validate_access(name) == 'admin':
+            username = request.form["username"]
+            if username in users:
+                del users[username]
+                return {"success": True, 'msg': f'User {username} deleted.'}
+            else:
+                return {"success": False, "error_msg": f"User {username} not found"}
+        else: return {'success': False, 'error_msg': 'No authentification for that action.'}
 
 class Auth(Resource):
     def post(self, name):
         for username, password in request.form.items():
             pw = password
         if users[name]["password"] != pw:
-            print("Login failed.")
             return {"success": False, "error_msg": "Wrong username or password."}
         else:
             tk = create_token()
             users[name]["token"] = tk
-            print("Successfully logged in.")
             return {"success": True, "error_msg": ""}
 
 
--- testing.py
+++ testing.py
@@ -1,6 +1,35 @@
 from requests import get, put, delete, post
 
+#%% login admin
 print(get('http://localhost:5000/users/api/admin').json())
 print(post('http://localhost:5000/users/api/session/admin', data={'password':'admin'}))
 print(get('http://localhost:5000/users/api/admin').json())
 
+#%% create user: Welcome on board, Ted! :-)
+print(post('http://localhost:5000/users/api/admin', data={'username': 'Ted', 'password':'somesupersafepassword', 'role': 'manager'}))
+print(get('http://localhost:5000/users/api/Ted').json())
+
+#%% delete user: Ted got fired again :(
+print(get('http://localhost:5000/users/api/Ted').json())
+print(delete('http://localhost:5000/users/api/admin', data = {'username': 'Ted'}))
+print(get('http://localhost:5000/users/api/Ted').json())
+
+#%% hire Ted again, Ted tries to hire Berta, but only admin can do that
+# admin logs in
+print(post('http://localhost:5000/users/api/session/admin', data={'password':'admin'}).json())
+print(post('http://localhost:5000/users/api/admin', data={'username': 'Ted','password':'somesupersafepassword', 'role': 'manager'}).json())
+print(get('http://localhost:5000/users/api/Ted').json())
+# Ted logs in
+print(post('http://localhost:5000/users/api/session/Ted', data={'password':'somesupersafepassword'}).json())
+# Ted tries to hire Berta
+print(post('http://localhost:5000/users/api/Ted', data={'username': 'Berta', 'password':'1234', 'role': 'secretary'}).json())
+print(get('http://localhost:5000/users/api/Berta').json())
+# admin hires Berta
+print(post('http://localhost:5000/users/api/admin', data={'username': 'Berta', 'password':'1234', 'role': 'secretary'}).json())
+print(get('http://localhost:5000/users/api/Berta').json())
+# Ted got mad and wants to fire Berta again
+print(delete('http://localhost:5000/users/api/Ted', data = {'username': 'Berta'}).json())
+print(get('http://localhost:5000/users/api/Berta').json())
+# Admin fires Berta again
+print(delete('http://localhost:5000/users/api/admin', data = {'username': 'Berta'}).json())
+print(get('http://localhost:5000/users/api/Berta').json())
