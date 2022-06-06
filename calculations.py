ee32c97b160efde10958841894068cd3570a10b3
sainioh <59020178+sainioh@users.noreply.github.com>
2022-06-06 11:16:11 +0200

    added comments and some logging

--- calculations.py
+++ calculations.py
@@ -17,17 +17,23 @@ n_proc = int(config_file['Processors']["n_proc"])
 app = Flask(__name__)
 api = Api(app)
 
-timeseries = create_time_series(100,300)
+timeseries = create_time_series(100,300)  # create time series data
 if not os.path.isfile("config.ini"):
     # if config file does not exist, create it
     create_config.create_config_file()
 
 def process_calc(assets):
+    '''
+    Predicts next time step for given asset.
+    :param assets:
+    :return: Average of next predicted time steps
+    '''
+
     comm = MPI.COMM_WORLD
     scatter_tasks = None
 
     if comm.rank == 0:
-        tasks = [json.dumps({"parameter":asset}) for asset in assets]
+        tasks = [json.dumps({"parameter":asset}) for asset in assets]  # create tasks from assets
 
         scatter_tasks = [None] *comm.size
         current_proc = 0
@@ -47,17 +53,14 @@ def process_calc(assets):
         for unit in units:
             asset = json.loads(unit)["parameter"]
 
-            model = linear_fit(timeseries[asset])
-            pred = predict_value(model, asset)
+            model = linear_fit(timeseries[asset])  # create model for asset
+            pred = predict_value(model, asset)  # predict next timestep
 
-            #pred = [pred, comm.rank]
             predictions.append(pred)
 
     # gathering results
     result = comm.gather(predictions, root=0)[0]
-
     return sum(result) / len(result)
-
 
 
 
@@ -69,7 +72,7 @@ class Calculations(Resource):
         if auth["success"] == False:
             return auth  # access denied
         results = []
-        while True:
+        while True:  # pull jobs until queue is empty
             u = get('http://localhost:7500/queues/api/manage').json()
             active_queues = u["msg"]
             loc = int(request.form["queue"]) # integer value indicating index of queue
@@ -81,14 +84,14 @@ class Calculations(Resource):
                 job = q.pop(0) # pull job
                 delete('http://localhost:7500/queues/api/queue', data={"queue": loc}).json()
                 logger.info(f'Job {job} was pulled from queue {loc}.')
+
                 j_id = list(job.keys())[0]
                 assets = job[j_id]["assets"]
-                result = process_calc(assets)
+                result = process_calc(assets)  # calculate results with mpi
+                logger.info(f'Calculations for job id  {j_id} were processed.')
+                # update result queue
                 put('http://localhost:7500/queues/api/queue', data={"job_id": j_id, "result": result})
-                #models = [linear_fit(timeseries[asset]) for asset in assets]
-                #preds = [predict_value(models[assets.index(x)], x) for x in assets]
-                #result = sum(preds)/len(preds)
-                #return {"success": True, "msg": result}
+
                 results.append(result)
                 print({"success": True, "msg": results})
             else:
@@ -97,8 +100,6 @@ class Calculations(Resource):
 
 
 api.add_resource(Calculations, '/financials/api/calc')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=7600)
